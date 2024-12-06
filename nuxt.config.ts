f17dd69c4dfa531d042df1f2ddf1158b8b596676
Koo In Young <inzerokoo@gmail.com>
2024-12-06 13:33:46 +0900

    config: Add plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@vueuse/nuxt', '@nuxt/eslint'],
+  nitro: {
+    experimental: {
+      websocket: true,
+    }
+  }
 })
