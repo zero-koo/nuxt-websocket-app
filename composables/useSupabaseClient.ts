3f8de22f7bd31104753ea36ac8a42694a2f0072b
Koo In Young <inzerokoo@gmail.com>
2024-12-09 15:30:17 +0900

    fix: test supabase client initialization

--- composables/useSupabaseClient.ts
+++ composables/useSupabaseClient.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-let supabase: ReturnType<typeof createClient> | null = null
+// let supabase: ReturnType<typeof createClient> | null = null
 
 export const useSupabaseClient = () => {
-  if (supabase !== null) return supabase
+  // if (supabase !== null) return supabase
   const {SUPABASE_URL, SUPABASE_KEY} = useRuntimeConfig().public
 
-  supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
   
   return supabase
 }
