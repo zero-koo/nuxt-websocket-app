c489fd76a9d4ac615f4000f8f90e2949905d4c86
Koo In Young <inzerokoo@gmail.com>
2024-12-10 12:58:54 +0900

    feat: Enhance colors

--- components/moon-light-board.vue
+++ components/moon-light-board.vue
@@ -26,6 +26,17 @@ const INITIAL_STATE: (Stone | null)[][] = [
   ['W_brown', 'W_green', 'W_red', 'W_yellow', 'W_pink', 'W_purple', 'W_blue', 'W_orange'],
 ]
 
+const COLOR_MAP: Record<Color, string> = {
+  orange: '#FFB347',
+  blue: '#91C3DF',
+  purple: '#B19CD9',
+  pink: '#F4A6A6',
+  yellow: '#FDFD96',
+  red: '#FF6961',
+  green: '#77DD77',
+  brown: '#D1B6A1',
+}
+
 const state = ref<(Stone | null)[][]>(INITIAL_STATE.map(row => ([...row])))
 
 type Coord = {r: number, c: number}
@@ -56,8 +67,8 @@ onMounted(() => {
       <div
         v-for="cell, cIndex in row"
         :key="cIndex"
-        :style="`background-color: ${cell}`"
-        :class="`w-[12.5%] h-full flex justify-center items-center`"
+        :style="`background-color: ${COLOR_MAP[cell]}`"
+        class="w-[12.5%] h-full flex justify-center items-center"
         @click="() => {
           if (!selectedCoord) return
           if (state[rIndex][cIndex]) return
@@ -76,12 +87,12 @@ onMounted(() => {
         }"
       >
         <button
-          v-if="state[rIndex][cIndex]" :class="`flex items-center justify-center size-[80%] rounded-full shadow-xl focus:scale-110`" :style="`background-color: ${state[rIndex][cIndex].split('_')[0] === 'B' ? 'black' : 'white'}`"
+          v-if="state[rIndex][cIndex]" :class="`flex items-center justify-center size-[75%] rounded-full shadow-md shadow-gray-500 focus:scale-110 focus:contrast-150`" :style="`background-color: ${state[rIndex][cIndex].split('_')[0] === 'B' ? '#222' : '#fff'}`"
           @click.stop="() => selectedCoord = {r: rIndex, c: cIndex}"
           @blue="() => selectedCoord = null"
         >
-          <div class="flex items-center justify-center size-[70%] rounded-full" :style="`background-color: ${state[rIndex][cIndex].split('_')[1]}`">
-            <div v-if="lastMovedCoord?.r === rIndex && lastMovedCoord?.c === cIndex" class="size-[50%] rounded-full bg-blue-500" />
+          <div class="flex items-center justify-center shadow-inner shadow-gray-500 size-[70%] rounded-full" :style="`background-color: ${COLOR_MAP[state[rIndex][cIndex].split('_')[1]]}`">
+            <div v-if="lastMovedCoord?.r === rIndex && lastMovedCoord?.c === cIndex" class="size-[40%] rounded-full bg-blue-500" />
           </div>
         </button>
       </div>
